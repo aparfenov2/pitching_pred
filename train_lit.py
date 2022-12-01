448205bce9b5b437404da15c4c4403786b07ae5d
Your Name <you@example.com>
2022-12-01 14:45:15 +0500

    12.5 Hz steplr

--- train_lit.py
+++ train_lit.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import numpy as np
+import random
 import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
@@ -18,16 +19,17 @@ class MyModel(nn.Module):
 
     def __init__(self):
         super(MyModel, self).__init__()
-        self.lstm1 = nn.LSTMCell(1, 51)
-        self.lstm2 = nn.LSTMCell(51, 51)
-        self.linear = nn.Linear(51, 1)
+        self.hidden_sz = 51
+        self.lstm1 = nn.LSTMCell(1, self.hidden_sz)
+        self.lstm2 = nn.LSTMCell(self.hidden_sz, self.hidden_sz)
+        self.linear = nn.Linear(self.hidden_sz, 1)
 
     def forward(self, input, future = 0):
         outputs = []
-        h_t = torch.zeros(input.size(0), 51, dtype=torch.float32)
-        c_t = torch.zeros(input.size(0), 51, dtype=torch.float32)
-        h_t2 = torch.zeros(input.size(0), 51, dtype=torch.float32)
-        c_t2 = torch.zeros(input.size(0), 51, dtype=torch.float32)
+        h_t = torch.zeros(input.size(0), self.hidden_sz, dtype=torch.float32)
+        c_t = torch.zeros(input.size(0), self.hidden_sz, dtype=torch.float32)
+        h_t2 = torch.zeros(input.size(0), self.hidden_sz, dtype=torch.float32)
+        c_t2 = torch.zeros(input.size(0), self.hidden_sz, dtype=torch.float32)
 
         for input_t in input.split(1, dim=1):
             h_t, c_t = self.lstm1(input_t, (h_t, c_t))
@@ -74,30 +76,42 @@ class LitPitchingPred(LightningModule):
 
     def training_epoch_end(self, training_step_outputs):
 
-        HZ = 16.66
+        HZ = self.HZ
 
         test_dl = self.trainer.val_dataloaders[0]
-        for _,y in test_dl:
-            break
-        y = y[0] # first series in batch
+        # test_dl = self.trainer.train_dataloader
+
+        random_batch = random.randint(0, len(test_dl))
+        for i,(_,y) in enumerate(test_dl):
+            if i >= random_batch:
+                break
+        # y = y[0] # first series in batch
+        y = random.choice(y)
         # ds = test_dl.dataset
-        heat_s = 12 # heaing 6 sec
+
+        heat_s = 30 # heaing 6 sec
         pred_s = 5
+        offset_s = 20
+        window_s = 20
 
         heat = y[:int(heat_s * HZ)]
 
         pred = self.model(heat[None,...], future=len(y)-len(heat))
         pred = pred[0].detach().numpy()
 
+        y = y[int(offset_s*HZ) : int((offset_s + window_s)*HZ)]
+        pred = pred[int(offset_s*HZ) : int((offset_s + window_s)*HZ)]
+
         # draw few samples
         fig = mpl.figure.Figure(figsize=(6, 4), dpi=100)
         fig.tight_layout(pad=0)
         canvas = FigureCanvas(fig)
         ax = fig.gca()
-        ax.axvline(heat_s * HZ, color='b', ls='dashed')
-        ax.axvline((heat_s + pred_s) * HZ, color='b', ls='dashed')
+        ax.axvline((heat_s - offset_s) * HZ, color='b', ls='dashed')
+        ax.axvline((heat_s + pred_s - offset_s) * HZ, color='b', ls='dashed')
         ax.plot(pred, color='r')
         ax.plot(y, color='g')
+        ax.set_title(f"Epoch {self.current_epoch} HZ={HZ:3.2f}")
 
         canvas.draw()
         img = np.frombuffer(canvas.tostring_rgb(), dtype='uint8')
@@ -106,6 +120,7 @@ class LitPitchingPred(LightningModule):
         img = img.swapaxes(0, 2).swapaxes(1, 2) # CHW
         # img = np.asarray(canvas.buffer_rgba())
         # img = img.reshape(canvas.get_width_height()[::-1] + (3,))
+        # self.logger.experiment.add_image(f'test_img_{self.current_epoch}', img)
         self.logger.experiment.add_image('test_img', img)
 
         # plt.savefig('predict%d.pdf'%i)
@@ -129,13 +144,17 @@ class MyDataset(Dataset):
         return self.data[idx], self.data_shifted[idx]
 
 
+HZ = 50/4
 
 class MyDataModule(LightningDataModule):
     def __init__(self, batch_size: int = 32):
         super().__init__()
         L = 1000
         data = pd.read_csv("NPN_1155_part2.dat", sep=" ")
-        data = data["KK"].values[::3] # 16,66 Hz
+        data = data["KK"].values
+        gaps = [140580, 177660, 520700]
+        data = data[:gaps[0]-1]
+        data = data[::4] # HZ !
         data = data[:(len(data)//L)*L]
         data = data.reshape(-1, L).astype('float32')
         dataset = MyDataset(data)
@@ -158,6 +177,8 @@ class MyDataModule(LightningDataModule):
 def cli_main():
     cli = LightningCLI(LitPitchingPred, MyDataModule, seed_everything_default=1234, save_config_overwrite=True, run=False)
     cli.model.set_datamodule(cli.datamodule)
+    cli.model.HZ = HZ
+
     cli.trainer.fit(cli.model, datamodule=cli.datamodule)
     # cli.trainer.test(ckpt_path="best", datamodule=cli.datamodule)
     # predictions = cli.trainer.predict(ckpt_path="best", datamodule=cli.datamodule)
